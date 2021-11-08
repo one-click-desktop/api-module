4292625ad80c85c515b13467ec5b4128cf8f6286
venmolom <p.widomski@gmail.com>
2021-11-08 19:41:04 +0100

    export fixes

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ API module for OneClickDesktop frontend application
 ## Generate
 
 Run `generate.bat` or `generate.sh` to generate Angular API module based on `overseer.yaml` definition.
+
+Because of errors with generation files `package.json` and `index.ts` are excluded from changes.
+If you need to regenerate them you can remove the entry from `.swagger-codegen-ignore` and restore it after generation. You may need to manually reapply the fixes.
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-export * from './api/api';
-export * from './model/models';
-export * from './variables';
-export * from './configuration';
-export * from './api.module';
+export {LoginService, MachinesService, ResourcesService, SessionService, APIS} from './api/api';
+export {IpAddress, Login, MachineType, Machines, ModelError, Resource, Resources, Server, Session, SessionStatus, Token, TotalResources} from './model/models';
+export {BASE_PATH, COLLECTION_FORMATS} from './variables';
+export {Configuration, ConfigurationParameters} from './configuration';
+export {ApiModule} from './api.module';
