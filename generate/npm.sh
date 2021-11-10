fc6dfedd58c8abbcc9b1de4dfff972aa17058c67
venmolom <p.widomski@gmail.com>
2021-11-10 18:03:43 +0100

    fix npm pipe

--- generate/npm.sh
+++ generate/npm.sh
@@ -3,20 +3,18 @@
 SOURCE=overseer.yaml
 OUTPUT=npm
 OPTIONS=options/npm.json
-VERSION=`awk '/version:/ {print $2}' $SOURCE`
 
 rm -rf $OUTPUT
 
-java -jar swagger-codegen-cli.jar generate \
+openapi-generator-cli generate \
  -i $SOURCE \
- -l typescript-angular \
+ -g typescript-angular \
+ -o $OUTPUT \
  -c $OPTIONS \
- --additional-properties npmVersion=VERSION \
- -o $OUTPUT
 
 if [ "$(ls -A patches/npm)" ]
 then
 	for i in patches/npm/* ; do
 	   patch -d $OUTPUT < $i
-	done 
+	done
 fi
