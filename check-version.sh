99ed6f976a913cfe9e4bf02246d8c6231839f70b
venmolom <p.widomski@gmail.com>
2021-11-10 18:20:05 +0100

    api version check fix

--- check-version.sh
+++ check-version.sh
@@ -4,10 +4,12 @@ SOURCE=overseer.yaml
 SOURCE_VERSION=$(curl -s https://raw.githubusercontent.com/one-click-desktop/api-module/$BRANCH/$SOURCE | awk '/version:/ {print $2}')
 CURRENT_VERSION=$(awk '/version:/ {print $2}' $SOURCE)
 
-echo Source version: $SOURCE_VERSION
-echo Current version: $CURRENT_VERSION
+if [ "$(echo $MODIFIED | grep $SOURCE)" ]; then
+    echo Source version:    $SOURCE_VERSION
+    echo Current version:   $CURRENT_VERSION
 
-if [ "$(printf '%s\n' "$CURRENT_VERSION" "$SOURCE_VERSION" | sort -Vr | head -n1)" = "$SOURCE_VERSION" ]; then
-    echo "API version needs to be higher than the one in main"
-    exit 1
+    if [ "$(printf '%s\n' "$CURRENT_VERSION" "$SOURCE_VERSION" | sort -Vr | head -n1)" = "$SOURCE_VERSION" ]; then
+        echo "API version needs to be higher than the one in main"
+        exit 1
+    fi
 fi
